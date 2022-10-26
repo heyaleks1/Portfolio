d890c95965cbc6d2771f32909e59243bbbd25078
heyaleks1 <heyaleks21@gmail.com>
2022-10-26 22:15:04 +1030

    a

---
+++ secure_email_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="#" id="form" method="post" name="form" class="form-1">  
+        <fieldset>
+        <div><input name="vname" class="input-1" type="text" name="name" placeholder="Name:"></div>
+        <div><input name="vemail" class="input-1" type="email" name="email" placeholder="Email:"></div>
+        <div><textarea name="message" class="input-1" rows="3" cols="30" placeholder="Message:"></textarea></div> 
+        <input id="send" name="" type="submit" value="Send Form" />
+        </fieldset>
+        </form>  
+</body>
+</html>
