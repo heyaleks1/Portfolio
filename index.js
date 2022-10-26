cf2b7bac0829e02be327accc744cf229cbacdf62
heyaleks1 <heyaleks21@gmail.com>
2022-10-26 20:47:35 +1030

    a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(port, () => {
