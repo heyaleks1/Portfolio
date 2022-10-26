8deecc6d40d890a2124b20d5e05ed03596c5fdb1
heyaleks1 <heyaleks21@gmail.com>
2022-10-26 22:06:44 +1030

    a

---
+++ secure_email_code.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="contact-sec">
+        <div class="contact-container">
+        <h1 id="contact">CONTACT</h1>
+        <p class="p-bold">Please fill in the form below to contact me.</p>
+        </div>
+        <form action="#" id="form" method="post" name="form" class="form-1">  
+        <fieldset>
+        <div><input name="vname" class="input-1" type="text" name="name" placeholder="Name:"></div>
+        <div><input name="vemail" class="input-1" type="email" name="email" placeholder="Email:"></div>
+        <div><textarea name="message" class="input-1" rows="3" cols="30" placeholder="Message:"></textarea></div> 
+        <input id="send" name="" type="submit" value="Send Form" />
+        </fieldset>
+        </form>  
+    </section> 
+</body>
+</html>
