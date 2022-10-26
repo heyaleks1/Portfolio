5bb7456facf0e5b8cbb594712d3907ea7e1559ac
heyaleks1 <heyaleks21@gmail.com>
2022-10-26 22:26:37 +1030

    a

--- secure_email_form.php
+++ secure_email_form.php
@@ -7,12 +7,34 @@
     <title>Document</title>
 </head>
 <body>
-<form action="#" id="form" method="post" name="form" class="form-1">  
+    <?php
+        if(!empty($_POST["send"])) {
+            $name = $_POST["name"];
+            $name = $_POST["email"];
+            $name = $_POST["message"];
+            $toEmail = $_POST["oceboosting@gmail.com"];
+
+            $mailHeaders = "Name: " . $name . 
+            "\r\n Name: " . $name . 
+            "\r\n Email: " . $email . 
+            "\r\n Message: " . $message . "\r\n";
+            
+            if(mail($email, $name, $mailHeaders)) {
+               $message = "Your message has been sent.";     
+            }
+        }
+    ?>
+<form method="post" name="form" class="form-1">  
         <fieldset>
-        <div><input name="vname" class="input-1" type="text" name="name" placeholder="Name:"></div>
-        <div><input name="vemail" class="input-1" type="email" name="email" placeholder="Email:"></div>
+        <div><input name="name" class="input-1" type="text" placeholder="Name:"></div>
+        <div><input name="email" class="input-1" type="email" placeholder="Email:"></div>
         <div><textarea name="message" class="input-1" rows="3" cols="30" placeholder="Message:"></textarea></div> 
         <input id="send" name="" type="submit" value="Send Form" />
+        <?php if(!empty($message)) { ?>
+            <div class="success">
+                <strong><?php echo $message; ?></strong>
+        </div>
+        <?php } ?>
         </fieldset>
         </form>  
 </body>
