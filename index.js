7620c20dfab9e2dfe6231a60dbf36ebc8fb575b2
heyaleks1 <heyaleks21@gmail.com>
2022-10-27 20:39:38 +1030

    a

--- index.js
+++ index.js
@@ -13,17 +13,50 @@ app.listen(port, () => {
 app.route('/').get((req, res) => {
     res.sendFile(process.cwd() + "/index.html")
 })
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+      user: "heyaleks22222@gmail.com",
+      pass: "ayzdutcumameulem",
+    },
+  });
 
-//get the form by its id
-const form = document.getElementById("form"); 
-
-//1.
-const formEvent = form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  //2.
-  let mail = new FormData(form);
-
-  //3.
-  sendMail(mail);
-})
+// verify connection configuration
+transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+  
+  app.post("/send", (req, res) => {
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+      console.log(data);
+      const mail = {
+        sender: `${data.name} <${data.email}>`,
+        to: "oceboosting@gmail.com", // receiver email,
+        subject: "New form",
+        text: `${data.name} <${data.email}> \n${data.message}`,
+      };
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Something went wrong.");
+        } else {
+          res.status(200).send("Email successfully sent to recipient!");
+        }
+      });
+    });
+  });
+  
+  //Index page (static HTML)
+  app.route("/").get(function (req, res) {
+    res.sendFile(process.cwd() + "/public/index.html");
+  });
