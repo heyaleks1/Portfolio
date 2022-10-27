3011871cf8839f5689f6ede343d7fcf4d64bcb7e
heyaleks1 <heyaleks21@gmail.com>
2022-10-27 20:35:24 +1030

    a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
 
 app.use(express.static('public'));
 
@@ -8,6 +10,20 @@ app.listen(port, () => {
     console.log('Listening on port 3000')
 })
 
-app.get('/aleks', (req, res) => {
-    res.send("aleks")
+app.route('/').get((req, res) => {
+    res.sendFile(process.cwd() + "/index.html")
+})
+
+//get the form by its id
+const form = document.getElementById("form"); 
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
 })
