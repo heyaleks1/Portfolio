7e61e2c17cc9c1cca6cedeba78320f77e499f839
heyaleks1 <heyaleks21@gmail.com>
2022-10-27 19:54:29 +1030

    a

--- secure_email_code.php
+++ secure_email_code.php
@@ -1,36 +1,37 @@
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
 <?php
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
-require "vendor/autoload.php";
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-//Creating a new instance, passing true enables exceptions
-$mail = new PHPMailer(true);
-
-//Server settings (gmail)
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-$mail->Username = "heyaleks22222";
-$mail->Password = "389a43c4";
-
-//Recipients
-$mail->setFrom($email, $name);
-$mail->addAddress("oceboosting@gmail.com", "Aleks"); //Add a recipient, name optional
-
-//Content
-$mail->Subject = $name;
-$mail->Body = $message;
-
-$mail->send();
-header("Location: sent.html");
+    function send_mail($email, $recipient_name, $message='')
+    {
+        require("phpmailer/class.phpmailer.php");
+    
+        $mail = new PHPMailer();
+    
+        $mail->CharSet = "utf-8";
+        $mail->IsSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = "mail.example.com";  // Specify main and backup server
+        $mail->SMTPAuth = true;     // Turn on SMTP authentication
+        $mail->Username = "heyaleks22222";  // SMTP username
+        $mail->Password = "389a43c4"; // SMTP password
+    
+        $mail->From = "oceboosting@gmail.com";
+        $mail->FromName = "Aleks";
+        $mail->AddAddress($email, $recipient_name);
+    
+        $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+        $mail->IsHTML(true);                                  // Set email format to HTML (true) or plain text (false)
+    
+        $mail->Subject = $email;
+        $mail->Body    = $message;
+        $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+        $mail->AddEmbeddedImage('images/logo.png', 'logo', 'logo.png');
+        $mail->addAttachment('files/file.xlsx');
+    
+        if(!$mail->Send())
+        {
+           echo "Message could not be sent. <p>";
+           echo "Mailer Error: " . $mail->ErrorInfo;
+           exit;
+        }
+    
+        echo "Message has been sent";
+    }
+?>
